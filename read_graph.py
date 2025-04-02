e70342ae1a05d9eff05a3f93e58be6d59767a720
Peter Blohm <e11905150@student.tuwien.ac.at>
2025-04-02 10:35:00 +0200

    Implement bad Gaec

--- read_graph.py
+++ read_graph.py
@@ -4,6 +4,7 @@ import networkx as nx
 import os
 import h5py
 
+# from graph_utils.graph_embeddings.data.fast_gd_embedding import adjacency_matrix
 from graph_utils.signed_graph_heuristics import find_max_ratio_vertex
 from graph_utils.signed_graph import SignedGraph, read_signed_graph
 from graph_utils.signed_graph_kernelization import kernelize_graph
@@ -65,9 +66,68 @@ def chicken_algorithm(G: SignedGraph):
     return vio
 
 
+def gaec(G: SignedGraph, k = None):
+    """ Takes a signed graph as input and computes a greedy additive edge contraction"""
+    # k is the forced number of clusters
+
+    num_vertex = max(G.G_plus.nodes()) + 1
+    clusters = -np.ones(num_vertex, dtype=int) #index map
+
+    # edges = [(1,u,v) for (u,v) in G.G_plus.edges() ] + [(-1,u,v) for (u,v) in G.G_minus.edges()]
+    edges = np.ones((num_vertex, num_vertex)) * -np.inf
+    node_list = list(G.G_plus.nodes())
+    print(node_list)
+    print(list(G.G_plus.nodes()))
+    for v in G.G_plus.nodes():
+        for v1 in G.G_plus.nodes():
+            edges[v,v1] = 0
+            edges[v1,v] = 0
+        clusters[v] = v
+        edges[v,v] = -np.inf
+
+    for (u,v) in G.G_plus.edges():
+        edges[u,v] = 1
+        edges[v,u] = 1
+        assert clusters[u] != clusters[v]
+
+    for (u,v) in G.G_minus.edges():
+        edges[v,u] = -1
+        edges[u,v] = -1
+        assert clusters[u] != clusters[v]
+        # looks weird but takes care of (non-consecutive) indices
+    ## take edge, slice clusters and edges
+    while (k and len(np.unique(clusters)) > k) or (not k and edges.max()>0):
+        weight, (alive_cluster, dead_cluster) = edges.max(), np.unravel_index(edges.argmax(), edges.shape)
+        if k < 50:
+            print(weight, (alive_cluster, dead_cluster), len(np.unique(clusters)))
+        alive_cluster, dead_cluster = min(alive_cluster, dead_cluster), max(alive_cluster, dead_cluster)
+        assert weight > -np.inf
+        # print((clusters == alive_cluster).sum(),
+        #       (clusters == dead_cluster).sum())
+        clusters[clusters == dead_cluster] = alive_cluster
+
+        # "If I need to, I'll go through you and absorb your fricking powers" ~ alive_cluster, probably
+        edges[alive_cluster,:] += edges[dead_cluster,:]
+        edges[:,alive_cluster] += edges[:,dead_cluster]
+        edges[dead_cluster,:] = -np.inf
+        edges[:,dead_cluster] = -np.inf
+        edges[alive_cluster, alive_cluster] = -np.inf
+
+    print(clusters[list(G.G_plus.nodes())])
+    obj = 0
+    for (u,v) in G.G_plus.edges():
+        obj += clusters[u] != clusters[v]
+
+    for (u,v) in G.G_minus.edges():
+        obj += clusters[u] == clusters[v]
+    print(obj)
+
+
+
+
 if __name__ == "__main__":
     #for file in os.listdir("data"):
-    data = f"data/soc-sign-Slashdot090221.txt"
+    data = f"/home/peter/tu/stefan_project/opinion_formation/data/soc-sign-Slashdot090221.txt"
 
     G = read_signed_graph(data)
 
@@ -80,7 +140,9 @@ if __name__ == "__main__":
     # Kernelize the graph
     graphs = kernelize_graph(G, safe=True)
     largest_graph = max(graphs, key=lambda graph: graph.number_of_nodes())
-
+    G.G_plus = nx.relabel.convert_node_labels_to_integers(G.G_plus, first_label=1, ordering='default')
+    G.G_minus = nx.relabel.convert_node_labels_to_integers(G.G_minus, first_label=1, ordering='default')
+    gaec(G,5)
     print(f"Number of vertices in largest connected component after 1 round of error free kernelization: {largest_graph.number_of_nodes()}")
     print(f"Number of positives edges in largest connected component after 1 round of error free kernelization: {largest_graph.G_plus.number_of_edges()}")
     print(f"Number of negative edges in largest connected component after 1 round of error free kernelization: {largest_graph.G_minus.number_of_edges()}")
