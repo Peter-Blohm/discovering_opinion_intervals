8ea852a4801ce9c7476f1317c3473e768be29607
Peter Blohm <e11905150@student.tuwien.ac.at>
2025-05-03 15:48:54 +0200

    Add Local Search based on vertex ordering

--- heuristics/src/main.rs
+++ heuristics/src/main.rs
@@ -7,12 +7,14 @@ use serde_json::json;
 mod data_types;
 mod algorithms;
 mod gaic;
+mod mars;
 
 use data_types::{SignedGraph, IntervalStructure};
 use algorithms::{greedy_additive_edge_contraction, cc_compute_violations, cc_local_search, brute_force_interval_structure};
 use gaic::greedy_absolute_interval_contraction;
 use crate::data_types::UsefulSignedGraph;
 use crate::gaic::GaicConfig;
+use crate::mars::greedy_vertex_ordering_from_assignment;
 // TODO: Handle case where graph is not connected and few clusters are desired
 
 fn main() {
@@ -33,6 +35,7 @@ fn main() {
     if algorithm == "gaic" {
 
         let node_labels = greedy_absolute_interval_contraction(&signed_graph, &interval_structure, &config, seed as u64);
+        // greedy_vertex_ordering_from_assignment(&signed_graph,&node_labels, num_clusters, seed as u64);
         let mut label_count:Vec<usize> = vec![0;interval_structure.intervals.len()];
         for &label in &node_labels {
             label_count[label] +=1;
