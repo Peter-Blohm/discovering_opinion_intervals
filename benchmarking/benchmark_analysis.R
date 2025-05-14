151e4184715e0c3d19333409902f2741c7d0b995
Peter Blohm <e11905150@student.tuwien.ac.at>
2025-05-14 15:05:58 +0200

    Add chaotic plotting code

--- benchmarking/benchmark_analysis.R
+++ benchmarking/benchmark_analysis.R
@@ -21,6 +21,16 @@ raw_summary_data <-  read.csv("summary_2.csv") %>%
              instance == "wikiconflict"       ~ "2014053",
              instance == "wikisigned-k2"       ~ "712337",
            ),
+           vertices = case_when(
+             instance == "bitcoinotc"      ~ "5881",
+             instance == "bundestag"       ~ "1480",
+             instance == "chess"       ~ "7301",
+             instance == "elec"       ~ "7115",
+             instance == "epinions"       ~ "131580",
+             instance == "slashdot"       ~ "138587",
+             instance == "wikiconflict"       ~ "116836",
+             instance == "wikisigned-k2"       ~ "138587",
+           ),
            instance = case_when(
              instance == "bitcoinotc"      ~ "\\Bitcoin",
              instance == "bundestag"       ~ "\\Bundestag",
@@ -56,7 +66,7 @@ raw_summary_data %>% group_by(instance,algorithm) %>%
           col.names = c("Dataset", "Best",   "\\text{Avg}p\\text{Std}", "Best",   "\\text{Avg}p\\text{Std}"),
           align     = c("l", "r", "D{p}{\\pm}{6.4}", "r", "D{p}{\\pm}{6.4}"))%>%
     collapse_rows(columns = 1, row_group_label_position= "identity", latex_hline              = "none") %>%
-    add_header_above(c(" " = 1, "\\texttt{Gaia}" = 2, "\\texttt{Venus}" = 2),escape=F) %>%
+    add_header_above(c(" " = 1, "\\texttt{GAIA}" = 2, "\\texttt{VENUS}" = 2),escape=F) %>%
     kable_styling(latex_options = c("hold_position")) %>%
     as.character() %>%
     cat(sep = "\n")
@@ -72,7 +82,7 @@ library(readr)
 #       rbindlist(idcol = "file")        # fread() is default reader inside rbindlist
 # full_slashdot_data <- DT[, file := tools::file_path_sans_ext(basename(file))]
 # type, num_clusters, algorithm, seed
-files <- list.files(path = "logs2/epinions", pattern = "\\.csv$", full.names = TRUE)
+files <- list.files(path = "logs2/slashdot", pattern = "\\.csv$", full.names = TRUE)
 full_slashdot_data <- do.call(
   rbind, lapply(files, function(f) {
     df <- read.csv(f, stringsAsFactors = FALSE)
@@ -102,7 +112,7 @@ filled_data <- full_slashdot_data %>%
     group_by(algorithm, num_clusters, type, seed, chunks,runtime) %>%
     filter(row_number()==1) %>%
     group_by(algorithm, num_clusters, type, seed, chunks) %>%
-    complete(runtime = seq(0, 120000, by=100)) %>%
+    complete(runtime = seq(0, 130000, by=100)) %>%
     arrange(runtime) %>%
     fill(best,.direction="downup") %>%
     filter(!is.na(best)) %>%
@@ -115,14 +125,14 @@ library(ggplot2)
 theme <- theme_minimal() +
     theme(legend.position = "bottom",
           legend.direction="horizontal",
-          legend.text = element_text(size = 16),  # Adjust legend text size
+          legend.text = element_text(size = 10),  # Adjust legend text size
           legend.key.width = unit(0.75, "cm"),  # Adjust width of legend keys (color boxes)
           legend.key.height = unit(0.5, "cm"),
-          axis.text = element_text(size = 14),
-          axis.title = element_text(size = 16),
-          plot.title = element_text(size = 18, face = "bold"),
-          strip.text = element_text(size = 16, vjust =2), # Left-align facet labels
-          axis.text.y = element_text(size = 14, hjust = 0), # Right-align y-axis labels
+          axis.text = element_text(size = 10),
+          axis.title = element_text(size = 10),
+          plot.title = element_text(size = 10, face = "bold"),
+          strip.text = element_text(size = 10, vjust =2), # Left-align facet labels
+          axis.text.y = element_text(size = 10, hjust = 0), # Right-align y-axis labels
           panel.grid.minor = element_blank(),
           # panel.grid.major = element_blank(),
           strip.placement = "outside",
@@ -137,34 +147,137 @@ summary_df <- filled_data %>%
   filter(chunks == 10, num_clusters %% 4 == 0, type=="intervals") %>%
   group_by(algorithm, num_clusters, runtime) %>%
   summarize(mean_obj = mean(obj, na.rm = TRUE),std_obj = sd(obj, na.rm = TRUE), .groups = "drop")
-g <- ggplot(summary_df %>% mutate(algorithm = factor(algorithm,levels=c("gaia","venus"),labels=c("Gaia","Venus"))), aes(x=runtime,y=mean_obj,color = algorithm, group=interaction(num_clusters,algorithm))) +
-  geom_line(aes(linetype=as.factor(num_clusters)),linewidth=1.5)+
-  geom_ribbon(aes(ymin = mean_obj - std_obj/2,max = mean_obj + std_obj/2,fill=algorithm,color=NULL,group=interaction(num_clusters,algorithm)),alpha=0)+
-  coord_cartesian(ylim=c(30000,40000)) + scale_color_manual(values=c("Gaia"="#00BFC4","Venus"="#F8766D")) +
-  xlim(0,120000) +theme + labs(x="Runtime",y="Avg Disagreement")
+g <- ggplot(summary_df %>%
+              mutate(algorithm = factor(algorithm,levels=c("gaia","venus"),labels=c("GAIA","VENUS"))),
+            aes(x=runtime,y=mean_obj,color = algorithm, group=interaction(num_clusters,algorithm))) +
+  geom_line(aes(linetype=as.factor(num_clusters)),linewidth=0.8)+
+  geom_ribbon(aes(ymin = mean_obj - std_obj,max = mean_obj + std_obj,fill=algorithm,color=NULL,group=interaction(num_clusters,algorithm)),alpha=0.2)+
+  scale_y_continuous(
+    labels = label_number(
+      scale    = 1e-3,   # divide values by 1,000
+      suffix   = "k",    # add "k"
+      accuracy = 1       # rounds to whole numbers (e.g. 120k not 120.4k)
+    )
+  ) +
+  scale_x_continuous(
+    labels = label_number(
+      scale    = 1e-3,   # divide values by 1,000
+      suffix   = "s",    # add "k"
+      accuracy = 1,       # rounds to whole numbers (e.g. 120k not 120.4k),
+
+    )
+  ) +
+  coord_cartesian(ylim=c(42500,57500),xlim=c(0,120000)) +
+  scale_color_manual(name= "Algorithm", values=c("GAIA"="#00BFC4","VENUS"="#F8766D")) +
+  scale_fill_manual(name="Algorithm",values=c("GAIA"="#00BFC4","VENUS"="#F8766D")) +
+  scale_linetype_manual("Number of Intervals", values=c(`4`="solid",`8`="dashed",`12`="dotdash",`16`="dotted"))+
+  theme + labs(x="Runtime",y="Disagreement") +guides(
+    color     = guide_legend(order = 1, nrow = 1),
+    fill      = guide_legend(order = 1, nrow = 1),
+    linetype  = guide_legend(order = 2, nrow = 1)
+  ) + theme(legend.box = "vertical",legend.title = element_text(size=10),
+legend.margin        = margin(t = 0, r = 0, b = 0, l = 0, unit = "cm"),
+
+    # **Legend box margin**: space between stacked legends (if legend.box="vertical")
+    legend.box.margin    = margin(t = -.4, r = 0, b = 0, l = 0, unit = "cm"),
+
+    # **Spacing between legend keys** (horizontal and vertical)
+    legend.spacing.x     = unit(0, "cm"),
+    legend.spacing.y     = unit(0.1, "cm"),
+    # **Padding inside each key** (controls distance between symbol and key border)
+    legend.key_PADDING   = unit(0.0, "cm"),
+
+    # **Key size** (height/width of each legend key box)
+    legend.key.height    = unit(0.3, "cm"),
+    legend.key.width     = unit(0.8, "cm"),
+            plot.margin = margin(t = 5, r = 20, b = 5, l = 5, unit = "pt")
+  ) +
+  guides(linetype  = guide_legend(
+                  order    = 2,
+                  nrow     = 1,
+                  override.aes = list(linewidth = .8)  # increase line width in legend
+                  ,keywidth     = unit(.8, "cm")
+                ))
 
 
-ggsave(filename = "epinions_time_plot.pdf",
+
+ggsave(filename = "slashdot_time_plot.pdf",
        plot     = g,        # or explicitly your ggplot object3000F", "#FFED00", "#151518", "#009EE0")) +
        device   = cairo_pdf,          # better text handling & UTF-8 supporttheme + scale_linewidth_identity() +guides()
-       width    = 8,                 # in inches (adjust to taste)
-       height   = 6,                  # in inches
+       width    = 2.7*1.5,                 # in inches (adjust to taste)
+       height   = 1.5*1.5,                  # in inches
        units    = "in")
 
 
 
 
 g <- ggplot(full_slashdot_data %>% filter(chunks == 10, num_clusters %% 4 == 0, type=="intervals") %>%
-  group_by(algorithm, num_clusters,seed) %>% arrange(runtime) %>% filter(row_number()==n()) %>% mutate(algorithm = factor(algorithm,levels=c("gaia","venus"),labels=c("Gaia","Venus"))),
-       aes(y=best,fill=algorithm,x=as.factor(num_clusters),group=interaction(algorithm,num_clusters))) + geom_boxplot(notch=TRUE) + theme + scale_fill_manual(values=c("Gaia"="#00BFC4","Venus"="#F8766D")) +
-  coord_cartesian(ylim=c(30000,40000)) + labs(x="Number of Intervals",y="Disagreement at Convergence")
+  group_by(algorithm, num_clusters,seed) %>% arrange(runtime) %>% filter(row_number()==n()) %>% mutate(algorithm = factor(algorithm,levels=c("gaia","venus"),labels=c("GAIA","VENUS"))),
+       aes(y=best,fill=algorithm,x=as.factor(num_clusters),group=interaction(algorithm,num_clusters))) + geom_boxplot(notch=TRUE) + theme + scale_fill_manual(values=c("GAIA"="#00BFC4","VENUS"="#F8766D")) +
+  coord_cartesian(ylim=c(30000,40000)) + labs(x="Number of Intervals",y="Disagreement at Convergence") +
+  scale_y_continuous(
+    labels = label_number(
+      scale    = 1e-3,   # divide values by 1,000
+      suffix   = "k",    # add "k"
+      accuracy = 1       # rounds to whole numbers (e.g. 120k not 120.4k)
+    )
+  ) +
+  coord_cartesian(ylim=c(42500,57500)) +
+  scale_color_manual(name= "Algorithm", values=c("GAIA"="#00BFC4","VENUS"="#F8766D")) +
+  scale_fill_manual(name="Algorithm",values=c("GAIA"="#00BFC4","VENUS"="#F8766D")) +
+  scale_linetype_manual("Number of Intervals", values=c(`4`="solid",`8`="dashed",`12`="dotdash",`16`="dotted"))+
+  theme + labs(x="Number of Intervals",y="Disagreement") +guides(
+    color     = guide_legend(order = 1, nrow = 1),
+    fill      = guide_legend(order = 1, nrow = 1),
+    linetype  = guide_legend(order = 2, nrow = 1)
+  ) + theme(legend.box = "vertical",legend.title = element_text(size=10),
+legend.margin        = margin(t = 0, r = 0, b = 14, l = 0, unit = "pt"),
 
-ggsave(filename = "epinions_conv_plot.pdf",
+    # **Legend box margin**: space between stacked legends (if legend.box="vertical")
+    legend.box.margin    = margin(t = -.4, r = 0, b = 0, l = 0, unit = "cm"),
+
+    # **Spacing between legend keys** (horizontal and vertical)
+    legend.spacing.x     = unit(0, "cm"),
+    legend.spacing.y     = unit(0.1, "cm"),
+    # **Padding inside each key** (controls distance between symbol and key border)
+    legend.key_PADDING   = unit(0.0, "cm"),
+
+    # **Key size** (height/width of each legend key box)
+    legend.key.height    = unit(0.3, "cm"),
+    legend.key.width     = unit(0.8, "cm"),
+            plot.margin = margin(t = 5, r = 20, b = 5, l = 5, unit = "pt")
+  ) +
+  guides(linetype  = guide_legend(
+                  order    = 2,
+                  nrow     = 1,
+                  override.aes = list(linewidth = .8)  # increase line width in legend
+                  ,keywidth     = unit(.8, "cm")
+                ))
+
+ggsave(filename = "slashdot_conv_plot.pdf",
        plot     = g,        # or explicitly your ggplot object3000F", "#FFED00", "#151518", "#009EE0")) +
        device   = cairo_pdf,          # better text handling & UTF-8 supporttheme + scale_linewidth_identity() +guides()
-       width    = 8,                 # in inches (adjust to taste)
-       height   = 6,                  # in inches
+       width    = 2.7*1.5,                 # in inches (adjust to taste)
+       height   = 1.5*1.5,                  # in inches
+       units    = "in")
+
+
+ggsave(filename = "slashdot_time_plot.pdf",
+       plot     = g,        # or explicitly your ggplot object3000F", "#FFED00", "#151518", "#009EE0")) +
+       device   = cairo_pdf,          # better text handling & UTF-8 supporttheme + scale_linewidth_identity() +guides()
+       width    = 2.7*1.5,                 # in inches (adjust to taste)
+       height   = 2*1.5,                  # in inches
        units    = "in")
 
 
 
+
+
+x <- raw_summary_data %>%
+  group_by(instance,num_clusters,type,algorithm,seed,edges,vertices) %>%
+  arrange(runtime_ms) %>%
+  summarize(runtime = max(runtime_ms), objective = min(best)) %>%
+  group_by(instance,num_clusters,type,algorithm,edges,vertices) %>%
+  summarize(runtime = mean(runtime), objective = mean(objective))
+
+ggplot(x,aes(x=as.numeric(edges),y=runtime/1000,color=algorithm,fill=algorithm)) + geom_point() + geom_smooth(method="lm",alpha   = 0.2)
